fac059191451e330ad980957cfe8eb0235f99cfd
Marafon <theomarafonflor@gmail.com>
2024-07-21 19:04:28 -0300

    Finalização definitiva código SiteStreamlit.py

--- Manipulação de contratos/Site Streamlit.py
+++ Manipulação de contratos/Site Streamlit.py
@@ -6,31 +6,31 @@ con_domestica = doc('Manipulação de contratos/Contratos/CONTRATO - DOMESTICA.d
 con_padrao = doc('Manipulação de contratos/Contratos/CONTRATO - PADRAO.docx')
 con_permuta = doc('Manipulação de contratos/Contratos/CONTRATO - PERMUTA.docx')
 
-st.set_page_config(page_title='Preencher Contratos Fácil e Rápido.com.br')
+st.set_page_config(page_title='PreencherContratos.com.br')
 listaaquivos = os.listdir('Manipulação de contratos\Contratos')
 
 st.subheader('Contratos disponíveis:')
 ce = st.selectbox(label='Contratos:', options=listaaquivos)
 
-RAZAO_SOCIAL = st.text_input('RAZAO_SOCIAL', placeholder='Alldax')
-CNPJ_CONTRATANTE = st.text_input('CNPJ_CONTRATANTE',placeholder='XX. XXX. XXX/XXXX-XX')
-CEP_EMPRESA = st.text_input('CEP_EMPRESA', placeholder='XXXX-XXX')
-ENDERECO_EMPRESA = st.text_input('ENDERECO_EMPRESA', placeholder='Rua, Bairro, Cidade, Estado')
-NOME_AVALISTA = st.text_input('NOME_AVALISTA', placeholder='Joao dos Santos')
-CPF_AVALISTA = st.text_input('CPF_AVALISTA', placeholder='XXX.XXX.XXX-XX')
-ENDERECO_AVALISTA = st.text_input('ENDERECO_AVALISTA', placeholder='Rua, Bairro, Cidade, Estado')
-CEP_AVALISTA = st.text_input('CEP_AVALISTA', placeholder='XXXX-XXX')
-VALOR_MENSAL = st.text_input('VALOR_MENSAL', placeholder='2100,00')
-VALOR_POR_EXTENSO = st.text_input('VALOR_POR_EXTENSO', placeholder='Dois Mil e Cem')
-QTD_FUNC = st.text_input('QTD_FUNC', placeholder='250')
-VALOR_FUNC = st.text_input('VALOR_FUNC', placeholder='1.412')
-VALOR_POR_EXTENSO_FUNC = st.text_input('VALOR_POR_EXTENSO_FUNC', placeholder='Mil Quatrocentos e Doze')
-REGIME = st.text_input('REGIME', placeholder='Simples/Lucro Real/Lucro Presumido')
-QTD_FISCAL = st.text_input('QTD_FISCAL', placeholder='200')
-QTD_CONTABIL = st.text_input('QTD_CONTABIL',placeholder='375')
-VENCIMENTO = st.text_input('VENCIMENTO', placeholder='XX/XX/XXXX')
-DATA_VIGENCIA = st.text_input('DATA_VIGENCIA', placeholder='XX/XX/XXXX')
-DATA_ASSINATURA = st.text_input('DATA_ASSINATURA', placeholder='XX/XX/XXXX')
+RAZAO_SOCIAL = st.text_input('RAZAO_SOCIAL')
+ENDERECO_EMPRESA = st.text_input('ENDERECO_EMPRESA')
+CEP_EMPRESA = st.text_input('CEP_EMPRESA')
+CNPJ_CONTRATANTE = st.text_input('CNPJ_CONTRATANTE')
+NOME_AVALISTA = st.text_input('NOME_AVALISTA')
+CPF_AVALISTA = st.text_input('CPF_AVALISTA')
+ENDERECO_AVALISTA = st.text_input('ENDERECO_AVALISTA')
+CEP_AVALISTA = st.text_input('CEP_AVALISTA')
+VALOR_MENSAL = st.text_input('VALOR_MENSAL')
+VALOR_POR_EXTENSO = st.text_input('VALOR_POR_EXTENSO')
+QTD_FUN = st.text_input('QTD_FUN')
+VALOR_FUN = st.text_input('VALOR_FUN')
+VALOR_POR_EXTENSO_FUN = st.text_input('VALOR_POR_EXTENSO_FUN')
+REGIME = st.text_input('REGIME')
+QTD_FISCAL = st.text_input('QTD_FISCAL')
+QTD_CONTABIL = st.text_input('QTD_CONTABIL')
+VENCIMENTO = st.text_input('VENCIMENTO')
+DATA_VIGENCIA = st.text_input('DATA_VIGENCIA')
+DATA_ASSINATURA = st.text_input('DATA_ASSINATURA')
 
 campos_contrato = [
 RAZAO_SOCIAL,
@@ -43,9 +43,9 @@ ENDERECO_AVALISTA,
 CEP_AVALISTA,
 VALOR_MENSAL,
 VALOR_POR_EXTENSO,
-QTD_FUNC,
-VALOR_FUNC,
-VALOR_POR_EXTENSO_FUNC,
+QTD_FUN,
+VALOR_FUN,
+VALOR_POR_EXTENSO_FUN,
 REGIME,
 QTD_FISCAL,
 QTD_CONTABIL,
@@ -116,12 +116,12 @@ def contrato_clube():
                     paragrafo.text = paragrafo.text.replace("}}", '')
                 if 'RAZAO_SOCIAL' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('RAZAO_SOCIAL', f"{campos_contrato_final['RAZAO_SOCIAL']}")
-                if 'CNPJ_CONTRATANTE' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('CNPJ_CONTRATANTE', f"{campos_contrato_final['CNPJ_CONTRATANTE']}")
-                if 'CEP_EMPRESA' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('CEP_EMPRESA', f"{campos_contrato_final['CEP_EMPRESA']}")
                 if 'ENDERECO_EMPRESA' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('ENDERECO_EMPRESA', f"{campos_contrato_final['ENDERECO_EMPRESA']}")
+                if 'CEP_EMPRESA' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('CEP_EMPRESA', f"{campos_contrato_final['CEP_EMPRESA']}")
+                if 'CNPJ_CONTRATANTE' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('CNPJ_CONTRATANTE', f"{campos_contrato_final['CNPJ_CONTRATANTE']}")
                 if 'NOME_AVALISTA' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('NOME_AVALISTA', f"{campos_contrato_final['NOME_AVALISTA']}")
                 if 'CPF_AVALISTA' in paragrafo.text:
@@ -134,12 +134,12 @@ def contrato_clube():
                     paragrafo.text = paragrafo.text.replace('VALOR_MENSAL', f"{campos_contrato_final['VALOR_MENSAL']}")
                 if 'VALOR_POR_EXTENSO' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO', f"{campos_contrato_final['VALOR_POR_EXTENSO']}")
-                if 'QTD_FUNC' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('QTD_FUNC', f"{campos_contrato_final['QTD_FUNC']}")
-                if 'VALOR_FUNC' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('VALOR_FUNC', f"{campos_contrato_final['VALOR_FUNC']}")
-                if 'VALOR_POR_EXTENSO_FUNC' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO_FUNC', f"{campos_contrato_final['VALOR_POR_EXTENSO_FUNC']}")
+                if 'QTD_FUN' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('QTD_FUN', f"{campos_contrato_final['QTD_FUN']}")
+                if 'VALOR_FUN' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('VALOR_FUN', f"{campos_contrato_final['VALOR_FUN']}")
+                if 'VALOR_POR_EXTENSO_FUN' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO_FUN', f"{campos_contrato_final['VALOR_POR_EXTENSO_FUN']}")
                 if 'REGIME' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('REGIME', f"{campos_contrato_final['REGIME']}")
                 if 'QTD_FISCAL' in paragrafo.text:
@@ -174,6 +174,12 @@ def contrato_domestica():
                     paragrafo.text = paragrafo.text.replace("}}", '')
                 if 'RAZAO_SOCIAL' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('RAZAO_SOCIAL', f"{campos_contrato_final['RAZAO_SOCIAL']}")
+                if 'ENDERECO_EMPRESA' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('ENDERECO_EMPRESA', f"{campos_contrato_final['ENDERECO_EMPRESA']}")
+                if 'CEP_EMPRESA' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('CEP_EMPRESA', f"{campos_contrato_final['CEP_EMPRESA']}")
+                if 'CNPJ_CONTRATANTE' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('CNPJ_CONTRATANTE', f"{campos_contrato_final['CNPJ_CONTRATANTE']}")
                 if 'NOME_AVALISTA' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('NOME_AVALISTA', f"{campos_contrato_final['NOME_AVALISTA']}")
                 if 'CPF_AVALISTA' in paragrafo.text:
@@ -187,11 +193,11 @@ def contrato_domestica():
                 if 'VALOR_POR_EXTENSO' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO', f"{campos_contrato_final['VALOR_POR_EXTENSO']}")
                 if 'QTD_FUN' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('QTD_FUN', f"{campos_contrato_final['QTD_FUNC']}")
+                    paragrafo.text = paragrafo.text.replace('QTD_FUN', f"{campos_contrato_final['QTD_FUN']}")
                 if 'VALOR_FUN' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('VALOR_FUN', f"{campos_contrato_final['VALOR_FUNC']}")
+                    paragrafo.text = paragrafo.text.replace('VALOR_FUN', f"{campos_contrato_final['VALOR_FUN']}")
                 if 'VALOR_POR_EXTENSO_FUN' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO_FUN', f"{campos_contrato_final['VALOR_POR_EXTENSO_FUNC']}")
+                    paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO_FUN', f"{campos_contrato_final['VALOR_POR_EXTENSO_FUN']}")
                 if 'REGIME' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('REGIME', f"{campos_contrato_final['REGIME']}")
                 if 'QTD_FISCAL' in paragrafo.text:
@@ -226,12 +232,12 @@ def contrato_padrão():
                     paragrafo.text = paragrafo.text.replace("}}", '')
                 if 'RAZAO_SOCIAL' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('RAZAO_SOCIAL', f"{campos_contrato_final['RAZAO_SOCIAL']}")
-                if 'CNPJ_CONTRATANTE' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('CNPJ_CONTRATANTE', f"{campos_contrato_final['CNPJ_CONTRATANTE']}")
-                if 'CEP_EMPRESA' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('CEP_EMPRESA', f"{campos_contrato_final['CEP_EMPRESA']}")
                 if 'ENDERECO_EMPRESA' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('ENDERECO_EMPRESA', f"{campos_contrato_final['ENDERECO_EMPRESA']}")
+                if 'CEP_EMPRESA' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('CEP_EMPRESA', f"{campos_contrato_final['CEP_EMPRESA']}")
+                if 'CNPJ_CONTRATANTE' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('CNPJ_CONTRATANTE', f"{campos_contrato_final['CNPJ_CONTRATANTE']}")
                 if 'NOME_AVALISTA' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('NOME_AVALISTA', f"{campos_contrato_final['NOME_AVALISTA']}")
                 if 'CPF_AVALISTA' in paragrafo.text:
@@ -244,12 +250,12 @@ def contrato_padrão():
                     paragrafo.text = paragrafo.text.replace('VALOR_MENSAL', f"{campos_contrato_final['VALOR_MENSAL']}")
                 if 'VALOR_POR_EXTENSO' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO', f"{campos_contrato_final['VALOR_POR_EXTENSO']}")
-                if 'QTD_FUNC' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('QTD_FUNC', f"{campos_contrato_final['QTD_FUNC']}")
-                if 'VALOR_FUNC' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('VALOR_FUNC', f"{campos_contrato_final['VALOR_FUNC']}")
-                if 'VALOR_POR_EXTENSO_FUNC' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO_FUNC', f"{campos_contrato_final['VALOR_POR_EXTENSO_FUNC']}")
+                if 'QTD_FUN' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('QTD_FUN', f"{campos_contrato_final['QTD_FUN']}")
+                if 'VALOR_FUN' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('VALOR_FUN', f"{campos_contrato_final['VALOR_FUN']}")
+                if 'VALOR_POR_EXTENSO_FUN' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO_FUN', f"{campos_contrato_final['VALOR_POR_EXTENSO_FUN']}")
                 if 'REGIME' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('REGIME', f"{campos_contrato_final['REGIME']}")
                 if 'QTD_FISCAL' in paragrafo.text:
@@ -284,12 +290,12 @@ def contrato_permuta():
                     paragrafo.text = paragrafo.text.replace("}}", '')
                 if 'RAZAO_SOCIAL' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('RAZAO_SOCIAL', f"{campos_contrato_final['RAZAO_SOCIAL']}")
-                if 'CNPJ_CONTRATANTE' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('CNPJ_CONTRATANTE', f"{campos_contrato_final['CNPJ_CONTRATANTE']}")
-                if 'CEP_EMPRESA' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('CEP_EMPRESA', f"{campos_contrato_final['CEP_EMPRESA']}")
                 if 'ENDERECO_EMPRESA' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('ENDERECO_EMPRESA', f"{campos_contrato_final['ENDERECO_EMPRESA']}")
+                if 'CEP_EMPRESA' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('CEP_EMPRESA', f"{campos_contrato_final['CEP_EMPRESA']}")
+                if 'CNPJ_CONTRATANTE' in paragrafo.text:
+                    paragrafo.text = paragrafo.text.replace('CNPJ_CONTRATANTE', f"{campos_contrato_final['CNPJ_CONTRATANTE']}")
                 if 'NOME_AVALISTA' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('NOME_AVALISTA', f"{campos_contrato_final['NOME_AVALISTA']}")
                 if 'CPF_AVALISTA' in paragrafo.text:
@@ -303,11 +309,11 @@ def contrato_permuta():
                 if 'VALOR_POR_EXTENSO' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO', f"{campos_contrato_final['VALOR_POR_EXTENSO']}")
                 if 'QTD_FUN' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('QTD_FUN', f"{campos_contrato_final['QTD_FUNC']}")
+                    paragrafo.text = paragrafo.text.replace('QTD_FUN', f"{campos_contrato_final['QTD_FUN']}")
                 if 'VALOR_FUN' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('VALOR_FUN', f"{campos_contrato_final['VALOR_FUNC']}")
+                    paragrafo.text = paragrafo.text.replace('VALOR_FUN', f"{campos_contrato_final['VALOR_FUN']}")
                 if 'VALOR_POR_EXTENSO_FUN' in paragrafo.text:
-                    paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO_FUN', f"{campos_contrato_final['VALOR_POR_EXTENSO_FUNC']}")
+                    paragrafo.text = paragrafo.text.replace('VALOR_POR_EXTENSO_FUN', f"{campos_contrato_final['VALOR_POR_EXTENSO_FUN']}")
                 if 'REGIME' in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace('REGIME', f"{campos_contrato_final['REGIME']}")
                 if 'QTD_FISCAL' in paragrafo.text:
